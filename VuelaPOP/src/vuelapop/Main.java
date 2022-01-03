c1262c1469f025be917e0bb257069cb8723e42a9
acarneirod <92722108+acarneirod@users.noreply.github.com>
2022-01-03 19:43:45 +0100

    Conexion con BBDD
    +Añadido postgresql-42.3.1.jar
    +El programa comprueba la conexión a la  base de datos VuelaPOP desde el usuario acarneiro

--- VuelaPOP/src/vuelapop/Main.java
+++ VuelaPOP/src/vuelapop/Main.java
@@ -5,9 +5,16 @@
  */
 package vuelapop;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Paulo
+ * @author Adrian Carneiro 
  */
 public class Main {
 
@@ -15,7 +22,12 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://easybyte.club:2224/VuelaPOP", "acarneiro", "acarneiro@Servo2021*")) {
+            System.out.println("Conexion realizada!");
+        }catch (SQLException e) {
+            System.out.println("Conexion fallida.");
+            e.printStackTrace();
+        }
     }
     
 }
